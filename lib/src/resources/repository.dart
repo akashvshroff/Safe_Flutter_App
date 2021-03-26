f6322961fce231e3896c76b52c3393a19bee0d69
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-26 13:22:44 +0530

    Added to-do for repo

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -6,4 +6,16 @@ import '../crypto_resources/random_password.dart';
 
 class Repository {
   final safeDbProvider = SafeDbProvider();
+
+  //check whether master password exists
+  //if not, create one via diceware, hash it and store and return
+
+  //check master password against stored hash
+
+  //fetch all the details
+
+  //convert encrypted password to password and vice-versa
+
+  //CRUD operations on the details
+
 }
