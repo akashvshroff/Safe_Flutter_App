0a4154e844d0e973089d4da5e884ea82dfc39fb2
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-26 13:14:57 +0530

    Boilerplate imports and instance variable

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,9 @@
+import 'safe_db_provider.dart';
+import '../crypto_resources/dice_ware.dart';
+import '../crypto_resources/encrypt_password.dart';
+import '../crypto_resources/hash_password.dart';
+import '../crypto_resources/random_password.dart';
+
+class Repository {
+  final safeDbProvider = SafeDbProvider();
+}
