c2e29644a551e0ccb4af10d5dba88ced745917b0
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-27 11:25:55 +0530

    Added all the repository functions

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,3 +1,5 @@
+import 'package:safe/src/models/detail_model.dart';
+
 import 'safe_db_provider.dart';
 import '../crypto_resources/dice_ware.dart';
 import '../crypto_resources/encrypt_password.dart';
@@ -6,16 +8,84 @@ import '../crypto_resources/random_password.dart';
 
 class Repository {
   final safeDbProvider = SafeDbProvider();
+  String _master;
 
-  //check whether master password exists
-  //if not, create one via diceware, hash it and store and return
+  Future<String> getMasterHash() async {
+    //returns master hash else null if not master
+    return await safeDbProvider.fetchMasterHash();
+  }
 
-  //check master password against stored hash
+  Future<String> createMasterPassword() async {
+    //creates new master password, adds hash to db and returns it
+    _master = getDiceWarePassword(n: 3);
+    String masterHash = getHash(_master);
+    safeDbProvider.addMasterHash(masterHash);
+    return _master;
+  }
 
-  //fetch all the details
+  Future<bool> checkMasterPassword(String masterInput) async {
+    //verifies inputted master password against hash in db
+    String masterHash = await safeDbProvider.fetchMasterHash();
+    if (verifyHash(masterInput, masterHash)) {
+      _master = masterInput;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  //convert encrypted password to password and vice-versa
+  Future<List<DetailModel>> fetchAllDetails() async {
+    //fetch all the details from the db
+    return await safeDbProvider.fetchAllDetails();
+  }
 
-  //CRUD operations on the details
+  Future<int> addDetail(String password, String service, String username) {
+    //add a fresh detail to the db
+    String encryptedPassword =
+        getEncryptedPassword(password, service, username);
+    DetailModel detail = DetailModel(
+        id: null,
+        service: service,
+        username: username,
+        encryptedPassword: encryptedPassword);
+    return safeDbProvider.addPassword(detail);
+  }
 
+  Future<int> updateDetail(String newPassword, DetailModel detail) {
+    //update a detail in the db
+    String encryptedPassword =
+        getEncryptedPassword(newPassword, detail.service, detail.username);
+    detail.encryptedPassword = encryptedPassword;
+    return safeDbProvider.updatePassword(detail);
+  }
+
+  Future<int> deleteDetail(int detailId) {
+    //delete a detail in the db
+    return safeDbProvider.deletePassword(detailId);
+  }
+
+  String getDiceWarePassword({n: 5}) {
+    //returns a diceware password of length n
+    return diceWarePassword(n);
+  }
+
+  String getRandomPassword({length: 16}) {
+    //random alphanum password
+    return randomPassword(length);
+  }
+
+  String getEncryptedPassword(
+      String password, String service, String username) {
+    //get encrypted password using master and AES
+    String key = _master + service + username;
+    key = key.substring(0, 32);
+    return encryptPassword(password, key);
+  }
+
+  String getDecryptedPassword(DetailModel detail) {
+    //get the original password from encrypted using master and AES
+    String key = _master + detail.service + detail.username;
+    key = key.substring(0, 32);
+    return decryptPassword(detail.encryptedPassword, key);
+  }
 }
