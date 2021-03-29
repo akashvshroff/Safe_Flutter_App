c2161478a189d86ded21507f7430fb498f77fce1
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-29 11:40:44 +0530

    Fixed UI Elements

--- lib/src/screens/details_list.dart
+++ lib/src/screens/details_list.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/material.dart';
 import '../blocs/provider.dart';
+import '../models/detail_model.dart';
+import '../widgets/detail_tile.dart';
 
 class DetailsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final bloc = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'safe.',
+        ),
+        centerTitle: true,
+      ),
+      body: buildList(bloc),
+      floatingActionButton: buildButton(context),
+    );
+  }
+
+  Widget buildList(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.details,
+      builder: (context, AsyncSnapshot<List<DetailModel>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final List<DetailModel> details =
+            snapshot.data != null ? snapshot.data : [];
+        return ListView.builder(
+          itemCount: details.length,
+          itemBuilder: (context, index) {
+            return DetailTile(detail: details[index]);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildButton(context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.pushNamed(context, '/add');
+      },
+      backgroundColor: Colors.blue,
+      child: Icon(Icons.add, color: Colors.white),
+    );
   }
 }
