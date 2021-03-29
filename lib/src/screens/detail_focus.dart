22b82313ef93c6e87353c0ea1df92c2564035cbd
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-29 11:48:40 +0530

    Boilerplate

---
+++ lib/src/screens/detail_focus.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/detail_model.dart';
+
+class DetailFocus extends StatelessWidget {
+  final DetailModel detail;
+
+  DetailFocus({this.detail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
