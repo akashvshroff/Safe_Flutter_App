831f6077934adc2568cedc46dc475ab51392bb68
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-04-01 15:23:43 +0530

    Renamed db

--- lib/src/resources/safe_db_provider.dart
+++ lib/src/resources/safe_db_provider.dart
@@ -16,7 +16,7 @@ class SafeDbProvider {
 
   Future init() async {
     var databasePath = await getDatabasesPath();
-    String path = join(databasePath, 'safe_details.db');
+    String path = join(databasePath, 'safe.db');
     db = await openDatabase(
       path,
       version: 1,
