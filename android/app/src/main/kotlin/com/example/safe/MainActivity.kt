a606abcb6d4668dc2a445ddaaee12ec5ea3d1c72
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-04-12 18:13:03 +0530

    Local auth dependencies

--- android/app/src/main/kotlin/com/example/safe/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/safe/MainActivity.kt
@@ -1,3 +1,6 @@
+package com.example.safe
+
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -7,3 +10,4 @@ class MainActivity: FlutterFragmentActivity() {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
+
